cef78d23bef47c06f02c64fabd831f3a9a37077e
devarifkhan <arifcse209@gmail.com>
2026-06-04 13:56:34 +0600

    Update author information in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author='deepranjan',
-    author_email='deepranjan@ineuron.ai',
+    author='devarifkhan',
+    author_email='arifcse209@gmail.com',
     packages=find_packages(),
     install_requires=[]
 )
