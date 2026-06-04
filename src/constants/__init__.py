3d2f286e680f247ffc44283496ce9bdd475e38e3
devarifkhan <arifcse209@gmail.com>
2026-06-04 12:08:19 +0600

    Refactor DEVICE initialization to handle import errors gracefully

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,14 +1,16 @@
 import os
-import torch
 from datetime import datetime
 
 # Common constants
 CONFIG_PATH: str = os.path.join(os.getcwd(), "config", "config.yaml")
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
-use_cuda = torch.cuda.is_available()
-DEVICE = torch.device("cuda:0" if use_cuda else "cpu")
 
+try:
+    import torch
+    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+except (ImportError, OSError):
+    DEVICE = "cpu"
 
 # Data ingestion constants
 DATA_INGESTION_ARTIFACTS_DIR = 'DataIngestionArtifacts'
